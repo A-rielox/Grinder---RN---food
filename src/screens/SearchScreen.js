6f04b53daa5792d54f52bd9ec4a7c75dc4caa04f
A-rielox <arielox.ag@gmail.com>
2023-09-15 18:08:33 -0600

    1ro

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SerchScreen = () => {
+   return (
+      <View>
+         <Text>Search Screen</Text>
+      </View>
+   );
+};
+
+const styles = StyleSheet.create({});
+
+export default SerchScreen;
